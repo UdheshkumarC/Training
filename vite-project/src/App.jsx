94f221e074c69ec331548d392c914193f6443372
UdheshkumarC <udheshkumar1536@gmail.com>
2025-02-15 18:31:42 +0530

    css part

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -2,16 +2,24 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+const App=() =>{
+  const myName="UDHESHKUMAR C";
+  const myAge=19;
+  React.useEffect(()=>{
+    alert("Hi Udheshkumar C");
+    console.log('My age is ${myAge}');
+   },[]);
+}
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <h1>UDHESHKUMAR C</h1>
+      <h1 style={{textAlign:"center",color:"cyan"}}>UDHESHKUMAR C</h1>
         
     </>
   )
 }
 
-export default App
+export default App;
